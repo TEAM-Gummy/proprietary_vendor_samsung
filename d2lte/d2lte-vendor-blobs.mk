ef2ea94839c32776b9690caf7ea12b38705fa77c
hedwig34 <hedwig34@gmail.com>
2014-02-17 00:19:43 -0500

    d2lte: remove propietary/lib_spi/        These are identical to lib_spr, so we'll just use those instead.
    Change-Id: Ib571905c9213616963c8d757de80d53fee3d546a

--- d2lte/d2lte-vendor-blobs.mk
+++ d2lte/d2lte-vendor-blobs.mk
@@ -143,8 +143,6 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2lte/proprietary/lib_att/libril.so:system/lib_att/libril.so \
         vendor/samsung/d2lte/proprietary/lib_r530/libril-qc-qmi-1.so:system/lib_r530/libril-qc-qmi-1.so \
         vendor/samsung/d2lte/proprietary/lib_r530/libril.so:system/lib_r530/libril.so \
-        vendor/samsung/d2lte/proprietary/lib_spi/libril-qc-qmi-1.so:system/lib_spi/libril-qc-qmi-1.so \
-        vendor/samsung/d2lte/proprietary/lib_spi/libril.so:system/lib_spi/libril.so \
         vendor/samsung/d2lte/proprietary/lib_spr/libril-qc-qmi-1.so:system/lib_spr/libril-qc-qmi-1.so \
         vendor/samsung/d2lte/proprietary/lib_spr/libril.so:system/lib_spr/libril.so \
         vendor/samsung/d2lte/proprietary/lib_tmo/libril-qc-qmi-1.so:system/lib_tmo/libril-qc-qmi-1.so \
